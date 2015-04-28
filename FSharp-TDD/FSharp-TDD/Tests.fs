07e0a1597fc40be98f1380bcae3f75533ed1fdc7
Ummon <greg.burri@gmail.com>
2015-04-28 12:16:36 +0200

    Begining of the unit tests.

---
+++ FSharp-TDD/FSharp-TDD/Tests.fs
@@ -0,0 +1,21 @@
+﻿namespace FSharpTDD
+
+open System
+open NUnit.Framework
+open FsUnit
+
+[<TestFixture>]
+type ``Element properties`` () =
+    [<Test>]
+    member this.``Create an element with correct properties`` () =
+        ()
+
+[<TestFixture>]
+type ``Elements in a shopping cart`` () =
+    [<Test>]
+    member this.``Can we add an element to a cart?`` () =
+        ()
+
+    [<Test>] 
+    member this.``Can we enumerate all items in a cart?`` () = 
+        ()
