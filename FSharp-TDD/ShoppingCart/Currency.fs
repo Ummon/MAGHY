3ddc2b47390169ebd4a3e4717bb51443819596f0
Ummon <greg.burri@gmail.com>
2015-04-29 22:31:09 +0200

    Add some tests+implementations for Currency.

---
+++ FSharp-TDD/ShoppingCart/Currency.fs
@@ -0,0 +1,63 @@
+﻿module Currency
+
+open System
+
+let mutable USD_CHF_rate = 0.95M // CHF per 1 USD.
+let mutable USD_XBT_rate = 0.00441M // Bitcoin per 1 USD.
+
+type Currency = USD | CHF | XBT
+
+let convert amount fromCurrency toCurrency =
+    match fromCurrency, toCurrency with
+    | USD, CHF -> amount * USD_CHF_rate
+    | CHF, USD -> amount / USD_CHF_rate
+    | USD, XBT -> amount * USD_XBT_rate
+    | XBT, USD -> amount / USD_XBT_rate
+    | _ -> amount
+
+[<CustomComparison; CustomEquality>]
+type Money = 
+    { Amount : decimal; Currency : Currency }
+
+    member this.Convert currency = { Amount = convert this.Amount this.Currency currency; Currency = currency }
+
+    interface IComparable<Money> with
+        member this.CompareTo other =
+            compare (this.Convert USD).Amount (other.Convert USD).Amount
+
+    interface IComparable with
+        member this.CompareTo obj =
+            match obj with
+            | :? Money as other -> (this :> IComparable<_>).CompareTo other
+            | _ -> invalidArg "obj" "not a Money"
+
+    interface IEquatable<Money> with
+        member this.Equals other =
+            (this.Convert USD).Amount = (other.Convert USD).Amount
+
+    override this.Equals obj =
+        match obj with
+        | :? Money as other -> (this :> IEquatable<_>).Equals other
+        | _ -> invalidArg "obj" "not a Money"
+
+    override this.GetHashCode () =
+        hash (this.Convert USD).Amount
+
+    static member (*) (m: Money, factor: decimal) : Money =
+        { m with Amount = m.Amount * factor }
+
+    static member (*) (factor: decimal, m: Money) : Money =
+        m * factor
+
+    static member (+) (m1: Money, m2: Money) =
+        if m1.Currency <> m2.Currency
+        then { Amount = (m1.Convert USD).Amount + (m2.Convert USD).Amount; Currency = USD }
+        else { m1 with Amount = m1.Amount + m2.Amount }
+
+    static member (-) (m1: Money, m2: Money) =
+        if m1.Currency <> m2.Currency
+        then { Amount = (m1.Convert USD).Amount - (m2.Convert USD).Amount; Currency = USD }
+        else { m1 with Amount = m1.Amount - m2.Amount }
+
+    override this.ToString () = 
+        sprintf "%A %A" this.Amount this.Currency
---
+++ FSharp-TDD/ShoppingCart/Tests.fs
@@ -0,0 +1,63 @@
+﻿namespace ShoppingCart.Tests
+
+open System
+open NUnit.Framework
+open FsUnit
+
+open ShoppingCart
+open Currency
+
+[<TestFixture>]
+type ``ISBN-13`` () =
+    [<Test>]
+    member this.``Creating an ISBN from a correct string`` () =
+        let validISBN = "9781784391232"
+        (ISBN13 validISBN).ToString () |> should equal validISBN
+
+    [<Test>]
+    member this.``Trying to create ISBNs from incorrect strings`` () =
+        (fun () -> ISBN13 "9781784391233" |> ignore) |> should throw typeof<InvalidISBNString>
+        (fun () -> ISBN13 "" |> ignore) |> should throw typeof<InvalidISBNString>
+        (fun () -> ISBN13 "abcdef" |> ignore) |> should throw typeof<InvalidISBNString>
+        (fun () -> ISBN13 null |> ignore) |> should throw typeof<InvalidISBNString>
+
+[<TestFixture>]
+type ``Currency`` () =
+    do
+        USD_CHF_rate <- 0.9M
+        USD_XBT_rate <- 0.005M
+
+    [<Test>]
+    member this.``Equality`` () = 
+        { Amount = 12M; Currency = USD } |> should equal { Amount = 10.8M; Currency = CHF }
+        { Amount = 12M; Currency = USD } |> should not' (equal { Amount = 12M; Currency = CHF })
+
+    [<Test>]
+    member this.``Comparisons`` () =
+        { Amount = 3M; Currency = USD } > { Amount = 1M; Currency = USD } |> should be True
+        { Amount = 2M; Currency = CHF } > { Amount = 2.1M; Currency = USD } |> should be True
+        { Amount = 1M; Currency = XBT } <= { Amount = 200M; Currency = USD } |> should be True
+        { Amount = 1M; Currency = XBT } <= { Amount = 180M; Currency = CHF } |> should be True
+
+    [<Test>]
+    member this.``Matematical operations`` () =
+        { Amount = 3M; Currency = USD } + { Amount = 1M; Currency = USD } |> should equal { Amount = 4M; Currency = USD }
+        { Amount = 40M; Currency = USD } - { Amount = 9M; Currency = CHF } |> should equal { Amount = 30M; Currency = USD }
+        10M * { Amount = 9M; Currency = XBT } |> should equal { Amount = 90M; Currency = XBT }
+        { Amount = 5M; Currency = USD } - { Amount = 9M; Currency = CHF } * 2M |> should equal { Amount = -15M; Currency = USD }
+
+(*[<TestFixture>]
+type ``Elements`` () =
+    [<Test>]
+    member this.``Create an element with correct properties`` () =
+        let elem = Element ("Testing with F#", *)
+
+[<TestFixture>]
+type ``Elements in a shopping cart`` () =
+    [<Test>]
+    member this.``Can we add an element to a cart?`` () =
+        ()
+
+    [<Test>] 
+    member this.``Can we enumerate all items in a cart?`` () =
+        ()
